971560801af1a7d092e7c8c675538e2919281ca8
CarolY <mon_alisa75@yahoo.com>
2020-09-28 08:59:48 -0700

    Initial README file added.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# code-quiz-cy
+# 04: Web APIs: Code Quiz
+
+## INRODUCTION 
+
+This application is a timed coding quiz with multiple -choice questions.  It will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript.  
+
+## REQUIREMENT
+
+In the default page, this application must be able to execute a timer and execute multiple choice questions.  Upon completion, it will record user's score and retain high score information.
+
+Language used:
+* Html
+* CSS
+* JavaScript
+
+## STEPS
+
+## MAINTAINER 
+This is an individual assignment created by Carolina Yoedhana.
+* Github Link: 
+* Gitpage Link: 
--- script.js
+++ script.js
@@ -12,19 +12,19 @@ var highscoreEl = document.querySelector("#saveScores");
 //My timer ----- start on button "Start" click
 // after click- hide button and direction to start quizz
 
-startButton.addEventListener("click", setTime);
-    var timeLeft = 10;
+
+var timeLeft = 10;
     function setTime() {
         var timerInterval = setInterval(function() {
         timeLeft--;
         timerEl.textContent = timeLeft;
         
-            if(timeLeft === 0) {
-            clearInterval(timerInterval);
-            // sendMessage();
-            }
-        }, 1000);
-    }
+        if(timeLeft === 0) {
+        clearInterval(timerInterval);
+        // sendMessage();
+        }
+    }, 1000);
+}
 
 //Need to start after button click
 function toggleMessage(show){
@@ -36,63 +36,74 @@ function toggleMessage(show){
       
  }
 
+  function initialize(){
+    startButton.addEventListener("click", function(){
+        toggleMessage(false);
+        
+        setTime();
+        
+
+        // startQuiz();
+
+    });
+  }  
+  initialize();
+
+
 let currentIndex = 0;
 
 let quizAnswer = [
     {
       question: "Who invented JavaScript?",
       answers: [
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
-      ]
-      correctAnswer: "c"
+         "Douglas Crockford",
+         "Sheryl Sandberg",
+         "Brendan Eich",
+      ],
+      correctAnswer: "Brendan Eich"
     },
 
     {
       question: "Which one of these is a JavaScript package manager?",
       answers: [
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      ] 
-      correctAnswer: "c"
+         "Node.js",
+         "TypeScript",
+         "npm",
+      ],
+        correctAnswer: "npm"
     },
         /*additional questions
     {
       question: "Which tool can you use to ensure code quality?",
       answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
+         "Angular",
+         "jQuery",
+         "RequireJS",
+         "ESLint",
       },
-      correctAnswer: "d"
+      correctAnswer: "ESLint"
     }
     */
 
   ];
 
+
   quizAnswer[currentIndex];
   function startQuiz(){
 
-    
-
-
-
+    currentIndex++;
   }
 
 
 
-    //Sample Quiz Questions - should appear after button start quiz click.
-    //multiple choice need to have event listener
 
 
-  //recording score
-  localStorage.setItem('score', 'highscore');
 
-  //retirieve score and post to Highscore
-  let scoreHistory = localStorage.getItem('highscore');
+//   //recording score
+//   localStorage.setItem('score', 'highscore');
 
-  //clear score
-  localStorage.clear()
+//   //retirieve score and post to Highscore
+//   let scoreHistory = localStorage.getItem('highscore');
+
+//   //clear score
+//   localStorage.clear()
