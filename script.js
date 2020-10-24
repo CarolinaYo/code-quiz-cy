701fd9fd53442f55454ae0cb949ec96347a624e8
CarolY <mon_alisa75@yahoo.com>
2020-10-23 18:55:14 -0700

    corrected ids and classes to match index.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var timerEl = document.querySelector("#timer");
-var startEl = document.querySelector(".toggle");
+var startEl = document.querySelector(".content");
 
 var startButton = document.querySelector(".start-button");
 
@@ -17,20 +17,25 @@ var displayScoresEl = document.querySelector(".displayScores");
 var returnToQuizEl = document.querySelector("#returnToQuiz");
 var clearScoreEl = document.querySelector("#clearScores");
 
-var timeLeft = 20;
+var timeLeft = 60;
 function setTime() {
   var timerInterval = setInterval(function () {
     timeLeft--;
     timerEl.textContent = timeLeft;
 
+    
+    //******************************************
     if (timeLeft === -1) {
       timerEl.textContent = 0;
-      alert("You have run out of time!");
+
+      // alert("You have run out of time!");
+      //function endOfGame; ***** need to build!
+
       clearInterval(timerInterval);
     }
   }, 1000);
 }
-
+//****************************************
 function toggleMessage(show) {
   if (show) {
     startEl.classList.remove("hidden");
@@ -54,13 +59,13 @@ let quizSet = [
   {
     question: "JavaScript file has an extension of?",
     answers: ["a. .Java", "b. .js", "c. .javascript"],
-    correctAnswer: "b. .js"
+    correctAnswer: "b. .js",
   },
 
   {
     question: "Which fuction is used to parse a string into integer?",
     answers: ["a. Parse", "b. Int.Parse", "c. None"],
-    correctAnswer: "c. None"
+    correctAnswer: "c. None",
   },
 ];
 
@@ -81,12 +86,11 @@ function showQuiz() {
   for (let i = 0; i < quizSet[currentIndex].answers.length; i++) {
     li = document.createElement("li");
     li.textContent = quizSet[currentIndex].answers[i];
-    //error message -- 
+    //error message --
     li.dataset.correctAnswer =
       quizSet[currentIndex].answers[i] === quizSet[currentIndex].correctAnswer;
 
     li.addEventListener("click", function (event) {
-      // alert("click works");
       let isCorrectAnswer = event.target.dataset.correctAnswer;
 
       let line = document.querySelector("#line");
@@ -99,39 +103,59 @@ function showQuiz() {
       let correctMessage = document.createTextNode("Correct!");
       let incorrectMessage = document.createTextNode("Incorrect!");
 
-      
       if (isCorrectAnswer) {
         currentScore += 5;
         resultMsg.appendChild(correctMessage);
-
       } else {
         timeLeft = timeLeft - 5;
         resultMsg.appendChild(incorrectMessage);
       }
-     //need to hide current quiz --- which is at quizSet[currentIndex];
-     //need time delay;
-     showQuiz();
+
+      //**********************************************
+      //need to hide current quiz --- which is at quizSet[currentIndex];
+      //need time delay;
+      setTimeout(showQuiz(), 2000);
+      toggleMessage(false);
     });
 
     quizAnswers.appendChild(li);
   }
 
   currentIndex++;
+  //show the next quiz
+  toggleMessage(true);
 }
 
-function endOfGame(){
+// End of game message
+function endOfGame() {
+  //display currentScore
+ currentScoreEl.textContent = currentScore;
 
+  //take initial
+  //On event click, save initial and score to local storage and hide and cal finalPage
+// med click listerner------>
+  let scoreInfo = currentScore + initialsEl;
+  localStorage.setItem('saveScore', 'scoreInfo');
+  toggleMessage(true);
+  finalPage();
 }
 
-function finalPage(){
+function finalPage() {
+  toggleMessage(false);
+  //retrieve score and post to Highscore
+    let scoreHistory = localStorage.getItem('saveScore');
+//  display scoreHistory
+displayScoresEl.texContent= scoreHistory;
+ 
+//on click returnToQuiz button call ----->
+//need click listener
+function initialize();
 
+
+//on click clearScore---------->
+//need click listerner
+
+  //clear score
+    localStorage.clear
+  //
 }
-//recording score
-//   localStorage.setItem('score', 'highscore');
-
-//retirieve score and post to Highscore
-//   let scoreHistory = localStorage.getItem('score');
-
-//clear score
-//   localStorage.clear
-//
