d9188fac9dd4a0d3658a9c4278c20c195114000c
CarolY <mon_alisa75@yahoo.com>
2020-10-23 22:10:54 -0700

    assign boolean value to correct answer.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var initialsEl = document.querySelector("#initials");
 var currentScoreEl = document.querySelector("#currentScore");
 var saveScoreEl = document.querySelector("#saveScore");
 
-var highScoreEl = document.querySelector("#highScores");
+var highScoreEl = document.querySelector("#displayHighScore");
 var displayScoresEl = document.querySelector(".displayScores");
 var returnToQuizEl = document.querySelector("#returnToQuiz");
 var clearScoreEl = document.querySelector("#clearScores");
@@ -58,72 +58,82 @@ let currentIndex = 0;
 let quizSet = [
   {
     question: "JavaScript file has an extension of?",
-    answers: ["a. .Java", "b. .js", "c. .javascript"],
-    correctAnswer: "b. .js",
+    answers: [".Java", ".js", ".javascript"],
+    correctAnswer: 1,
   },
 
   {
     question: "Which fuction is used to parse a string into integer?",
-    answers: ["a. Parse", "b. Int.Parse", "c. None"],
-    correctAnswer: "c. None",
+    answers: ["Parse", "Int.Parse", "None"],
+    correctAnswer: 2,
   },
 ];
 
-quizSet[currentIndex];
+// quizSet[currentIndex];
 let currentScore = 0;
 
 function showQuiz() {
   let question = document.querySelector("#question");
   let div = document.createElement("div");
 
-  div.textContent = quizSet[currentIndex].question;
-  question.appendChild(div);
-
-  let quizAnswers = document.querySelector("#quizAnswers");
-
-  let li;
-
-  for (let i = 0; i < quizSet[currentIndex].answers.length; i++) {
-    li = document.createElement("li");
-    li.textContent = quizSet[currentIndex].answers[i];
-    //error message --
-    li.dataset.correctAnswer =
-      quizSet[currentIndex].answers[i] === quizSet[currentIndex].correctAnswer;
-
-    li.addEventListener("click", function (event) {
-      let isCorrectAnswer = event.target.dataset.correctAnswer;
-
-      let line = document.querySelector("#line");
-      let hr = document.createElement("hr");
-      let messageEl = document.querySelector("#message");
-      let resultMsg = document.createElement("p");
-      line.appendChild(hr);
-      messageEl.appendChild(resultMsg);
-
-      let correctMessage = document.createTextNode("Correct!");
-      let incorrectMessage = document.createTextNode("Incorrect!");
-
-      if (isCorrectAnswer) {
-        currentScore += 5;
-        resultMsg.appendChild(correctMessage);
-      } else {
-        timeLeft = timeLeft - 5;
-        resultMsg.appendChild(incorrectMessage);
-      }
-
-      //**********************************************
-      //need to hide current quiz --- which is at quizSet[currentIndex];
-      //need time delay;
-      setTimeout(showQuiz(), 2000);
-      toggleMessage(false);
-    });
-
-    quizAnswers.appendChild(li);
+  if (currentIndex < quizSet.length) {
+    div.textContent = quizSet[currentIndex].question;
+    question.appendChild(div);
+  
+    let quizAnswers = document.querySelector("#quizAnswers");
+  
+    let li;
+    quizAnswers.innerHTML="";
+  
+    for (let i = 0; i < quizSet[currentIndex].answers.length; i++) {
+      li = document.createElement("li");
+      // li.textContent = quizSet[currentIndex].answers[i];
+      
+      li.textContent = (i + 1) + '. ' + quizSet[currentIndex].answers[i];
+      //error message --
+      li.dataset.correctAnswer = (i === quizSet[currentIndex].correctAnswer);
+  
+      li.addEventListener("click", function (event) {
+        // console.log('Before correctAnswer: ', event.target.dataset.correctAnswer);
+        // console.log('After correctAnswer: ', !!event.target.dataset.correctAnswer);
+        let isCorrectAnswer = event.target.dataset.correctAnswer;
+  
+        let line = document.querySelector("#line");
+        let hr = document.createElement("hr");
+        let messageEl = document.querySelector("#message");
+        let resultMsg = document.createElement("p");
+        line.appendChild(hr);
+        messageEl.appendChild(resultMsg);
+  
+        let correctMessage = document.createTextNode("Correct!");
+        let incorrectMessage = document.createTextNode("Incorrect!");
+  
+        if (isCorrectAnswer) {
+          console.log('correct answer');
+          currentScore += 5;
+          resultMsg.appendChild(correctMessage);
+        } else {
+          console.log('wrong answer');
+          timeLeft = timeLeft - 10;
+          resultMsg.appendChild(incorrectMessage);
+        }
+  
+        console.log("is correct answer: ", isCorrectAnswer);
+        //**********************************************
+        //need to hide current quiz --- which is at quizSet[currentIndex];
+        //need time delay;
+        setTimeout(showQuiz(), 2000);
+        
+      });
+  
+      quizAnswers.appendChild(li);
+    }
+  
+    currentIndex++;
+    //show the next quiz
+    toggleMessage(true);
   }
 
-  currentIndex++;
-  //show the next quiz
-  toggleMessage(true);
 }
 
 // End of game message
@@ -133,14 +143,14 @@ function endOfGame() {
 
   //take initial
   //On event click, save initial and score to local storage and hide and cal finalPage
-// med click listerner------>
+  // need click listener------>
   let scoreInfo = currentScore + initialsEl;
   localStorage.setItem('saveScore', 'scoreInfo');
   toggleMessage(true);
-  finalPage();
+  finalDisplay();
 }
 
-function finalPage() {
+function finalDisplay() {
   toggleMessage(false);
   //retrieve score and post to Highscore
     let scoreHistory = localStorage.getItem('saveScore');
@@ -149,13 +159,13 @@ displayScoresEl.texContent= scoreHistory;
  
 //on click returnToQuiz button call ----->
 //need click listener
-function initialize();
+  // function initialize();
 
 
 //on click clearScore---------->
 //need click listerner
 
   //clear score
-    localStorage.clear
+    localStorage.clear;
   //
 }
